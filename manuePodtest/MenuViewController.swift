402cc11800e88fbbfab966909572830f894eec98
Takarkiz <ponpontanuki16@gmail.com>
2017-01-03 19:36:53 +0900

    first commit

---
+++ manuePodtest/MenuViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MenueViewController.swift
+//  manuePodtest
+//
+//  Created by 澤田昂明 on 2017/01/03.
+//  Copyright © 2017年 takarki. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController,GuillotineMenu {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func dismissButtonTapped(_ sender: UIButton) {
+        presentingViewController!.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func menuButtonTapped(_ sender: UIButton) {
+        presentingViewController!.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closeMenu(_ sender: UIButton) {
+        presentingViewController!.dismiss(animated: true, completion: nil)
+    }
+
+}
+
+extension MenuViewController: GuillotineAnimationDelegate {
+    
+    func animatorDidFinishPresentation(_ animator: GuillotineTransitionAnimation) {
+        print("menuDidFinishPresentation")
+    }
+    func animatorDidFinishDismissal(_ animator: GuillotineTransitionAnimation) {
+        print("menuDidFinishDismissal")
+    }
+    
+    func animatorWillStartPresentation(_ animator: GuillotineTransitionAnimation) {
+        print("willStartPresentation")
+    }
+    
+    func animatorWillStartDismissal(_ animator: GuillotineTransitionAnimation) {
+        print("willStartDismissal")
+    }
+}
---
+++ manuePodtest/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  manuePodtest
+//
+//  Created by 澤田昂明 on 2017/01/03.
+//  Copyright © 2017年 takarki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+    fileprivate lazy var presentationAnimator = GuillotineTransitionAnimation()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let navBar = self.navigationController!.navigationBar
+        navBar.barTintColor = UIColor(red: 65.0 / 255.0, green: 62.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
+        navBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+    }
+    
+    @IBAction func showMenuAction(_ sender: UIButton) {
+        let menuViewController = storyboard!.instantiateViewController(withIdentifier: "MenuViewController")
+        menuViewController.modalPresentationStyle = .custom
+        menuViewController.transitioningDelegate = self
+        
+        presentationAnimator.animationDelegate = menuViewController as? GuillotineAnimationDelegate
+        presentationAnimator.supportView = navigationController!.navigationBar
+        presentationAnimator.presentButton = sender
+        present(menuViewController, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
+
+extension ViewController: UIViewControllerTransitioningDelegate {
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        presentationAnimator.mode = .presentation
+        return presentationAnimator
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        presentationAnimator.mode = .dismissal
+        return presentationAnimator
+    }
+}
